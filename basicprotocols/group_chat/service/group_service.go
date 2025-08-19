64154195d8464c3baa71ac141733a3315c8cbc79
codepotter66 <kylehe22@gmail.com>
2025-08-19 09:16:53 +0800

    fix: fix bug

--- basicprotocols/group_chat/service/group_service.go
+++ basicprotocols/group_chat/service/group_service.go
@@ -416,7 +416,7 @@ func FetchGroupChatList(req *request.FetchGroupChatListRequest) (*respond.GroupC
 			Chain:       chat.Chain,
 			BlockHeight: chat.BlockHeight,
 		}
-		if strings.Contains(strings.ToLower(chatItem.Protocol), protocols.MonitorSimpleGroupOpenLuckyBag) {
+		if strings.Contains(strings.ToLower(chatItem.Protocol), strings.ToLower(protocols.MonitorSimpleGroupOpenLuckyBag)) {
 			openLuckyBag, _ := chatDB.GetOpenLuckyBagByPinId(chat.PinId)
 			if openLuckyBag != nil {
 				fmt.Printf("openLuckyBag: GrabTxId: %s, PinId: %s, GrabState: %d\n", openLuckyBag.GrabTxId, openLuckyBag.PinId, openLuckyBag.GrabState)
@@ -520,6 +520,7 @@ func FetchGroupChatListV2(req *request.FetchGroupChatListRequest) (*respond.Grou
 		if strings.Contains(strings.ToLower(chatItem.Protocol), strings.ToLower(protocols.MonitorSimpleGroupOpenLuckyBag)) {
 			openLuckyBag, _ := chatDB.GetOpenLuckyBagByPinId(chat.PinId)
 			if openLuckyBag != nil {
+				fmt.Printf("openLuckyBag: GrabTxId: %s, PinId: %s, GrabState: %d\n", openLuckyBag.GrabTxId, openLuckyBag.PinId, openLuckyBag.GrabState)
 				if openLuckyBag.GrabState == models.GrabStateOpenAndSend {
 					chatItem.TxId = openLuckyBag.GrabTxId
 				} else {
